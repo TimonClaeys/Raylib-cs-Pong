82bc0d78b516bc76fec242cb50bf9d1aada3bebb
TimonClaeys <timon.claeys@outlook.com>
2022-05-28 18:13:25 +0200

    Added game boilerplate.

---
+++ Pong/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+
+namespace Pong
+{
+    public class Game
+    {
+        public bool WindowShouldClose => Raylib.WindowShouldClose();
+
+        public Game(Vector2 size, int fps, string title)
+        {
+            Raylib.SetTargetFPS(60);
+            Raylib.InitWindow((int) size.X, (int) size.Y, title);
+        }
+
+        public void Tick()
+        {
+            Update();
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.RAYWHITE);
+            Draw();
+            Raylib.EndDrawing();
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void Draw()
+        {
+            
+        }
+    }
+}
---
+++ Pong/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+using Pong;
+
+Game game = new Game(new Vector2(1600, 900), 60, "PONG");
+
+while (!game.WindowShouldClose)
+{
+    game.Tick();
+}
